338d68a8f20192c99151fcc5677d71b30132848c
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2022-08-02 16:29:56 +0900

    Update 05. 이중 포인터.c

--- C언어 공부/05. 이중 포인터.c
+++ C언어 공부/05. 이중 포인터.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int a = 5;
+	int* p = &a;
+	int** pp = &p;   // **pp는 p의 주소가 가리키는 값을 가리킨다. *pp는 p의 값을 가리킨다.
+
+	printf("a의 값: %d, *p의 값: %d, **pp의 값: %d\n\n", a, *p, **pp);
+	printf("a의 주소값: %p, p의 값: %p, *pp의 값: %p\n\n", &a, p, *pp);
+	printf("p의 주소값: %p, pp의 값: %p\n", &p, pp);
+}
+
+출력결과 :
+		a의 값: 5, *p의 값: 5, **pp의 값: 5
+
+		a의 주소값: 00000051E68FF7D4, p의 값: 00000051E68FF7D4, *pp의 값: 00000051E68FF7D4
+
+		p의 주소값: 00000051E68FF7F8, pp의 값: 00000051E68FF7F8
+
+------------------------
 int main()
 {
 	int a = 5;

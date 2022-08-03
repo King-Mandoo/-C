09ef1ced4c1f14bcdb8f6cd5ab8e2695fdb0bc74
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2022-08-03 12:47:45 +0900

    Update 05. 이중 포인터.c

--- C언어 공부/05. 이중 포인터.c
+++ C언어 공부/05. 이중 포인터.c
@@ -43,3 +43,73 @@ int main()
 
 	return 0;
 }
+
+-------------------------------
+	
+#include <stdio.h>
+
+int pswap(int** pa, int** pb);
+
+int main() {
+	int a, b;
+	int* pa, * pb;
+
+	pa = &a;   // pa에 a의 주소값
+	pb = &b;   // pb에 b의 주소값
+
+	printf("pa 가 가리키는 변수의 주소값 : %p \n", pa);   // a의 주소
+	printf("pa 의 주소값 : %p \n \n", &pa);             // pa의 주소
+	printf("pb 가 가리키는 변수의 주소값 : %p \n", pb);   // b의 주소
+	printf("pb 의 주소값 : %p \n", &pb);         // pb의 주소
+	printf(" a 의 주소값 : %p \n", &a);
+	printf(" b 의 주소값 : %p \n", &b);
+
+	printf("\n ------------- 호출 -------------- \n");
+	pswap(&pa, &pb);
+	printf("\n ------------- 호출끝 -------------- \n");
+
+	printf("pa 가 가리키는 변수의 주소값 : %p \n", pa);   // b의 주소값
+	printf("pa 의 주소값 : %p \n \n", &pa);  // 그대로 pa의 주소
+	printf("pb 가 가리키는 변수의 주소값 : %p \n", pb);   // a의 주소값
+	printf("pb 의 주소값 : %p \n", &pb);     // 그대로 pb의 주소
+	printf(" a 의 주소값 : %p \n", &a);
+	printf(" b 의 주소값 : %p \n", &b);
+
+	return 0;  
+}
+
+int pswap(int** ppa, int** ppb) {
+	int* temp = *ppa;   // *ppa는 pa의 값(a의 주소)
+
+	printf("ppa 가 가리키는 변수의 주소값 : %p \n", ppa);  // pa의 주소
+	printf("ppb 가 가리키는 변수의 주소값 : %p \n", ppb);  // pb의 주소
+
+	*ppa = *ppb;   // *ppa에 pb의 값(b의 주소), *ppb에 pa의 값(a의 주소)
+	*ppb = temp;
+
+	return 0;
+}
+
+  출력결과: 
+		pa 가 가리키는 변수의 주소값 : 0000004E65FDFCA4
+		pa 의 주소값 : 0000004E65FDFCE8
+
+		pb 가 가리키는 변수의 주소값 : 0000004E65FDFCC4
+		pb 의 주소값 : 0000004E65FDFD08
+		 a 의 주소값 : 0000004E65FDFCA4
+		 b 의 주소값 : 0000004E65FDFCC4
+		 
+		 ------------- 호출 --------------
+		ppa 가 가리키는 변수의 주소값 : 0000004E65FDFCE8
+		ppb 가 가리키는 변수의 주소값 : 0000004E65FDFD08
+		 ------------- 호출끝 --------------
+			
+		pa 가 가리키는 변수의 주소값 : 0000004E65FDFCC4
+		pa 의 주소값 : 0000004E65FDFCE8
+
+		pb 가 가리키는 변수의 주소값 : 0000004E65FDFCA4
+		pb 의 주소값 : 0000004E65FDFD08
+		 a 의 주소값 : 0000004E65FDFCA4
+		 b 의 주소값 : 0000004E65FDFCC4
+		 
+		 
